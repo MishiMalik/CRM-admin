5a1041937823258cf46e82872e0d2c7847c29dc4
Mehwish <mehwishgul715@gmail.com>
2023-09-23 21:41:52 +0500

    commit

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,3 +1,4 @@
+// toggle theme
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("toggleTheme");
     const themeLink = document.getElementById("theme-link");
@@ -11,6 +12,37 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             themeLink.href = lightThemeURL;
             alert("Switching to light theme");
+        }
+    });
+});
+// expand screen
+$(document).ready(function() {
+    $("#fullscreen-button").on("click", function() {
+        var element = document.documentElement; // Get the HTML element
+
+        if (!document.fullscreenElement &&    // If no element is in full screen
+            !document.mozFullScreenElement && 
+            !document.webkitFullscreenElement && 
+            !document.msFullscreenElement) {  // Check if any fullscreen element is present
+            if (element.requestFullscreen) {
+                element.requestFullscreen(); // Enter fullscreen mode
+            } else if (element.mozRequestFullScreen) {
+                element.mozRequestFullScreen();
+            } else if (element.webkitRequestFullscreen) {
+                element.webkitRequestFullscreen();
+            } else if (element.msRequestFullscreen) {
+                element.msRequestFullscreen();
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen(); // Exit fullscreen mode
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
         }
     });
 });
