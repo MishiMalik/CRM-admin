4001c3b973d34d28f7f1e4ce519bcf3865ecbc26
Mehwish <mehwishgul715@gmail.com>
2023-09-27 22:04:49 +0500

    commit

--- assets/Js/table.js
+++ assets/Js/table.js
@@ -1,92 +1,96 @@
-   // Formatting function for row details - modify as you need
-   function format(d) {
-    // `d` is the original data object for the row
-    var tableHtml = `<div class="slider"><table class="extendedTable">
-        <thead>
-        <tr>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Attachments</th>
-        </tr>
-        </thead>
-        <tbody>`;
-
-    d.forEach((data) => {
-        tableHtml += `<tr>
-                <td>${data.date}</td>
-                <td>${data.description}</td>
-                <td><a href="${data.attachments}"><i class="bi-eye-fill"></i> VIEW</a></td>
-                </tr>`;
-    })
-
-    tableHtml += `</tbody></table>
-     
-    <div class="show-all-notifications-btn"><button>Show More <img src="assets/images/show-more.svg" class="ms-2"/> </button></div>
-    
-    </div>`;
-
-    return tableHtml;
-}
-
-const table = new DataTable('#example', {
-    // responsive: true
-    scrollX: true,
-    order: [[1, 'asc']],
-    columnDefs: [
-        { targets: 'no-sort', orderable: false },
-        {
-            className: 'dt-control',
-            orderable: false,
-            targets: 0,// Adjust the target column as needed
-
-        }
-
-    ],
+const table = $('#example').DataTable({
+    responsive: true,
     language: {
-        searchPlaceholder: "Search here..."
-    }
+                searchPlaceholder: "Search records"
+                
+            },
+});
+const exampleinprogress = $('#example-inprogress').DataTable({
+    responsive: true,
+    language: {
+                searchPlaceholder: "Search records"
+                
+            },
+});
+const tableFailed = $('#example-failed').DataTable({
+    responsive: true,
+    language: {
+                searchPlaceholder: "Search records"
+                
+            },
+});
+const exampleQualify = $('#example-qualify').DataTable({
+    responsive: true,
+    language: {
+                searchPlaceholder: "Search records"
+                
+            },
 });
 
-const data = [
-    { 'date': '20-09-2023', 'description': "Lorem Ipsum is simply dummy text of the dummy text ever since the 1500s", 'attachments': 'https://google.com' },
-    { 'date': '20-09-2023', 'description': "Lorem Ipsum is simply dummy text of the dummy text ever since the 1500s 2", 'attachments': 'https://youtube.com' },
-    { 'date': '20-09-2023', 'description': "Lorem Ipsum is simply dummy text of the dummy text ever since the 1500s", 'attachments': 'https://google.com' },
-    { 'date': '20-09-2023', 'description': "Lorem Ipsum is simply dummy text of the dummy text ever since the 1500s 2", 'attachments': 'https://youtube.com' },
-    { 'date': '20-09-2023', 'description': "Lorem Ipsum is simply dummy text of the dummy text ever since the 1500s 2", 'attachments': 'https://youtube.com' }
-]
-
-// Add event listener for opening and closing details
-table.on('click', 'td.dt-control', function (e) {
-    let tr = $(this).closest('tr');
-    let row = table.row(tr);
-
-    if (row.child.isShown()) {
-        // This row is already open - close it
-        $('div.slider', row.child()).slideUp(function () {
-            row.child.hide();
-            tr.removeClass('shown');
-
-            tr.find("td:first-child").removeClass("selected-col")
-            tr.removeClass("selected-row")
-        });
-    }
-    else {
-        // Open this row
-        row.child(format(data), 'no-padding').show();
-
-        tr.addClass('shown');
-
-        tr.find("td:first-child").addClass("selected-col")
-        tr.addClass("selected-row")
-
-        $('div.slider', row.child()).slideDown();
-
-        new DataTable($(this).closest('tr').next().find(".extendedTable"), {
-            info: false,
-            // ordering:false,
-            paging: false,
-            searching: false,
-            lengthChange: false
-        })
-    }
-});
+// // Formatting function for row details - modify as you need
+// function format(d) {
+//     // `d` is the original data object for the row
+//     return (`<div class="container-fluid">
+//         <div class="row">
+//             <div class="col-lg-6">
+//                 <div class="d-flex align-items-center mb-2">
+//                     <h2 class="table-caption">URL:</h2>
+//                     <h2 class="table-value"><a href="https://crm-admin123.netlify.app/">https://crm-admin123.netlify.app/</a></h2>
+//                 </div>
+//                 <div class="d-flex align-items-center mb-2">
+//                     <h2 class="table-caption">State/Country:</h2>
+//                     <h2 class="table-value">Pakistan</h2>
+//                 </div>
+//                 <div class="d-flex align-items-center mb-2">
+//                     <h2 class="table-caption">Address:  </h2>
+//                     <h2 class="table-value">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, voluptatibus?</h2>
+//                 </div>
+//                 <div class="d-flex align-items-center mb-2">
+//                     <h2 class="table-caption">Phone Number:   </h2>
+//                     <h2 class="table-value">0300 887 6652</h2>
+//                 </div>
+//                 <div class="d-flex align-items-center mb-2">
+//                     <h2 class="table-caption">Date: </h2>
+//                     <h2 class="table-value">12/8/2023</h2>
+//                 </div>
+//                 <div class="d-flex align-items-center mb-2">
+//                     <h2 class="table-caption">Follow-up Date:</h2>
+//                     <h2 class="table-value">12/8/2023</h2>
+//                 </div>
+//             </div>
+//         </div>
+//       </div>`);
+// }
+ 
+// let table = new DataTable('#example', {
+//     responsive: true,
+//     scrollX: true,
+//     language: {
+//         searchPlaceholder: "Search records"
+        
+//     },
+//     columnDefs: [
+//       {
+//         className: 'dt-control',
+//         orderable: false,
+//         data: null,
+//         defaultContent: '',
+//         targets: 0
+//       }
+//     ]
+// });
+ 
+// // Add event listener for opening and closing details
+// table.on('click', 'td.dt-control', function (e) {
+//     let tr = e.target.closest('tr');
+//     let row = table.row(tr);
+ 
+//     if (row.child.isShown()) {
+//         // This row is already open - close it
+//         row.child.hide();
+//     }
+//     else {
+//         // Open this row
+//         row.child(format(row.data())).show();
+//     }
+// });
