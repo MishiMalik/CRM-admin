fa16f74b5ff389e3cc3f6edd2cbcb4c253115965
Mehwish <mehwishgul715@gmail.com>
2023-09-30 20:14:07 +0500

    commit

--- assets/Js/chat.js
+++ assets/Js/chat.js
@@ -128,13 +128,14 @@ var initialScreenSize = window.innerHeight;
 window.addEventListener("resize", function() {
    if(window.innerHeight < initialScreenSize){
         $(".menu-footer").hide(); 
-   } else{ 
-        if($(window).width() < 580) {
-            $(".menu-footer").show(); 
-            $(".chat__screen").css("height","calc(100vh - 190px)"); 
+             $(".chat__screen").css("height","calc(100vh - 190px)"); 
             $(".chat__screen-chats").css("height","calc(100vh - 190px)"); 
             $(".chat__screen-chats-wrapper").css("height","calc(100vh - 250px)"); 
             $(".chat__screen-body-msgs").css("height","calc(100vh - 250px)"); 
+   } else{ 
+        if($(window).width() < 580) {
+            $(".menu-footer").show(); 
+       
         }
        
    } 
